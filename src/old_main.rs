82f469135d528a0632f7ebd73d068d42d16db7df
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-04 15:02:02 -0400

    add print

--- src/old_main.rs
+++ src/old_main.rs
@@ -145,6 +145,7 @@ fn setup_cgroup() {
     fs::create_dir("/sys/fs/cgroup/my_cgroup").expect("Failed to create cgroup");
     fs::write("/sys/fs/cgroup/my_cgroup/cgroup.type", "threaded").expect("Failed to set cgroup type");
     fs::write("/sys/fs/cgroup/my_cgroup/cgroup.subtree_control", "+cpu +cpuset").expect("Failed to set cgroup type");
+    print!("finished setup!\n");
 }
 
 fn set_thread_weight(thread_id: usize, weight: u32) {
